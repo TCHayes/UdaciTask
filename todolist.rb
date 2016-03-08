11c13d03acf50fb8452d8779c1ddf42c2528977e
TCHayes <terrychayes@gmail.com>
2016-03-08 13:43:33 -0500

    Wrote out basic structure of both files; made some guesses on methods I was unsure about. Technically should have made smaller changes and tested them along the way, but was nice to get things fully mapped out.

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,44 @@
 class TodoList
     # methods and stuff go here
+    def initialize(list_title)
+    	@title = list_title
+    	@items = Array.new
+    end
+
+    attr_reader :title, :items
+
+    def add_item(new_item)
+    	item = Item.new(new_item)
+    	@items.push(item)
+    end
+
+    def remove_item(item)
+        @items.delete(item) # Correct method?
+    end
+
+    def rename_list(new_name)
+        @title = new_name
+    end
+
+    def toggle_completion_status(finished_item)
+        finished_item[:completion_status] = !finished_item["completion_status"] # how to reference correct item and item attribute?
+    end
+
+    def print_list
+        puts @items #Still need to make the output look pretty
+    end
+
+
 end
 
 class Item
     # methods and stuff go here
+    def initialize(item_description)
+    	@description = item_description
+    	@completion_status = false
+    end
+
+    def completed?
+        @completion_status
+    end
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,37 @@
-require_relative 'todolist.rb'
+require_relative "todolist"
 
 # Creates a new todo list
+march_todos = TodoList.new("March To-do List")
 
 # Add four new items
+march_todos.add_item("Finish Udacity Task Manager")
+march_todos.add_item("Buy Groceries")
+march_todos.add_item("Finish Work Project")
+march_todos.add_item("Rebalance Investments")
 
 # Print the list
+march_todos.print_list
 
 # Delete the first item
+march_todos.remove_item([0])
 
 # Print the list
+march_todos.print_list
 
 # Delete the second item
+march_todos.remove_item([1])
 
 # Print the list
+march_todos.print_list
 
 # Update the completion status of the first item to complete
+march_todos[:items][0][:completion_status].toggle_completion_status
 
 # Print the list
+march_todos.print_list
 
 # Update the title of the list
+march_todos.rename_list("Early March To-dos")
 
 # Print the list
+march_todos.print_list
